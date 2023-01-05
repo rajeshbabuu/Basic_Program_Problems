7f839c18983d3b37aaaac12e876faccdb01165de
Rajesh kumar <rajeshbabu656656@gmail.com>
2023-01-05 18:06:49 +0530

    Print the table of Powers Of 2

--- Basicprograms/Program.cs
+++ Basicprograms/Program.cs
@@ -1,24 +1,19 @@
 ﻿namespace Basicprogram
 {
-    public class Leapyear
+    public class PowerOf2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the year in 4 digits: ");
-            int Year = Convert.ToInt32(Console.ReadLine());
-
-           if (((Year % 4 == 0) && (Year % 100 != 0)) || (Year % 400 == 0))
-           
-                {
-                Console.WriteLine("{0} is a Leap Year", Year);
-                
-            }
-            else
+            Console.WriteLine("Enter the power value: ");
+            long N = Convert.ToInt32(Console.ReadLine());
+            int num = 2;
+            for (var i = 0; i < N; i++)
             {
-                Console.WriteLine("{0} is not a Leap Year", Year); ;
+                Console.WriteLine(num);
+                num = num * 2;
             }
             Console.ReadLine();
-            
+           
 
             }
 
